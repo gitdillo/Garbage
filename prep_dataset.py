f2fdfa0e0ae8b09797c45b0490dbaaa00311d573
gitdillo <andreas.gazis@gmail.com>
2018-12-22 10:55:45 +0100

    Finished adding get_sliced_shapes, get_safe_crop_boundaries, slice_to_tiles, dataset_crop_to_ROI and dataset_to_tiles to image_utils. Added script prep_dataset which uses image_utils to prepare a full directory into cropped, tiled images and VOC xml annotation files.

---
+++ prep_dataset.py
@@ -0,0 +1,16 @@
+from image_utils import dataset_crop_to_ROI, dataset_to_tiles
+import os
+
+
+dataset_path = '/windows_storage/IT/Keras_YOLO/litter_data/Training_Data/Radanvagen_set_6-7_Dec_2019/Grass_B/'
+
+# First, crop the edges, that do not contain any shapes, away from the ROI
+roi_out_dir = os.path.join(dataset_path, 'roi_cropped')
+dataset_crop_to_ROI(dataset_path, roi_out_dir, roi_padding=10)
+
+
+# Second, chop up the cropped images into tiles
+tile_out_dir = os.path.join(dataset_path, 'crop2tiles')
+tile_width = 500
+tile_height = 500
+dataset_to_tiles(dataset_path, tile_width, tile_height, tile_out_dir)
